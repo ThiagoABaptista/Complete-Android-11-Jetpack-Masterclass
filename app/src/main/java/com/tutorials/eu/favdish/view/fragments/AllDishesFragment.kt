4d3a16dad3a820eb4155a318c649ce2bd07659a5
ThiagoABaptista <baptistathiago3@gmail.com>
2021-12-28 19:13:52 -0300

    Mostrando os pratos na tela principal e pequenas refatorações
    Agora quando o Prato é adicionado ele salva na memória e quando acessa a tela principal é carregado da memória todos os pratos e mostra eles como um card. Também alterei os botões da barras inferior, colocando um novo ícone e titulo para cada, mudando o id deles para refletir isso.

--- app/src/main/java/com/tutorials/eu/favdish/view/fragments/AllDishesFragment.kt
+++ app/src/main/java/com/tutorials/eu/favdish/view/fragments/AllDishesFragment.kt
@@ -10,17 +10,20 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.tutorials.eu.favdish.R
 import com.tutorials.eu.favdish.application.FavDishApplication
+import com.tutorials.eu.favdish.databinding.FragmentAllDishesBinding
 import com.tutorials.eu.favdish.model.entities.FavDish
 import com.tutorials.eu.favdish.view.activities.AddUpdateDishActivity
+import com.tutorials.eu.favdish.view.adapters.FavDishAdapter
 import com.tutorials.eu.favdish.viewmodel.FavDishViewModel
 import com.tutorials.eu.favdish.viewmodel.FavDishViewModelFactory
 import com.tutorials.eu.favdish.viewmodel.HomeViewModel
 
 class AllDishesFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var mBinding : FragmentAllDishesBinding
 
     // TODO Step 4: Create a ViewModel instance to access the methods.
     // START
@@ -44,14 +47,8 @@ class AllDishesFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_all_dishes, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        mBinding = FragmentAllDishesBinding.inflate(inflater,container,false)
+        return mBinding.root
     }
 
     // TODO Step 5: Override the onViewCreated method and get the dishes list and print the title in Log for now.
@@ -63,11 +60,23 @@ class AllDishesFragment : Fragment() {
          * O método onChanged() é acionado quando os dados
          * observados são alterados e a atividade está em primeiro plano.
          */
+
+        mBinding.rvDishesList.layoutManager = GridLayoutManager(requireActivity(),2)
+        val favDishAdapter = FavDishAdapter(this@AllDishesFragment)
+
+        mBinding.rvDishesList.adapter = favDishAdapter
+
         mFavDishViewModel.allDishesList.observe(viewLifecycleOwner) { dishes ->
             dishes.let {
 
-                for (item in it){
-                    Log.i("Dish Title", "${item.id} :: ${item.title}")
+                if(it.isNotEmpty()){
+                    mBinding.rvDishesList.visibility = View.VISIBLE
+                    mBinding.tvNoDishesAddedYet.visibility = View.GONE
+
+                    favDishAdapter.dishesList(it)
+                }else{
+                    mBinding.rvDishesList.visibility = View.GONE
+                    mBinding.tvNoDishesAddedYet.visibility = View.VISIBLE
                 }
             }
         }
