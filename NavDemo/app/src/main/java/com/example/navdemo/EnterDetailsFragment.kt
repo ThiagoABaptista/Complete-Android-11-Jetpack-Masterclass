dcecd8d7421d4b1ba9c1f150c4c763cc6a999e92
ThiagoABaptista <baptistathiago3@gmail.com>
2021-12-30 21:16:04 -0300

    Navigation Component Demo
    Uma pequena demo utilizando Navigation, Fragment e Safe Args. A aplicação em si somente mostra os dados que foram inseridos no fragmento inicial em outro fragmento.

---
+++ NavDemo/app/src/main/java/com/example/navdemo/EnterDetailsFragment.kt
@@ -0,0 +1,62 @@
+package com.example.navdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+
+
+class EnterDetailsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val rootView = inflater.inflate(R.layout.fragment_enter_details, container, false)
+
+        val etName = rootView.findViewById<EditText>(R.id.et_first_name)
+        val etMobileNumber = rootView.findViewById<EditText>(R.id.et_mobile)
+
+
+        val btnVerifyDetails = rootView.findViewById<Button>(R.id.btn_verify_details)
+        btnVerifyDetails.setOnClickListener{
+
+            val firstName = etName.text.toString()
+            val mobile = etMobileNumber.text.toString()
+
+            when{
+                firstName.isEmpty() -> {
+                    Toast.makeText(activity,"Enter Name.", Toast.LENGTH_SHORT).show()
+                }
+                mobile.isEmpty() -> {
+                    Toast.makeText(activity,"Enter Mobile Number.", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    findNavController().navigate(
+                        EnterDetailsFragmentDirections
+                            .actionEnterDetailsFragmentToVerifyDetailsFragment2(
+                                firstName,
+                                mobile.toLong()
+                            )
+                    )
+
+                }
+            }
+
+
+
+        }
+        return rootView
+    }
+}
