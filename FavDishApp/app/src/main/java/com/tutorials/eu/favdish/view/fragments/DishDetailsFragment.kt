791ccd2772bbffb21e3b03296965459a573cd929
ThiagoABaptista <baptistathiago3@gmail.com>
2022-01-13 12:08:40 -0300

    Usando Palette para alterar o Background do Detalhes dos pratos
    Basicamente, Agora a cor de fundo da tela de detalhes muda para a cor mais dominante da imagem do prato. Visualmente, não ficou muito agradável, então talvez eu retire essa funcionalidade depois.

--- FavDishApp/app/src/main/java/com/tutorials/eu/favdish/view/fragments/DishDetailsFragment.kt
+++ FavDishApp/app/src/main/java/com/tutorials/eu/favdish/view/fragments/DishDetailsFragment.kt
@@ -1,12 +1,20 @@
 package com.tutorials.eu.favdish.view.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.tutorials.eu.favdish.R
 import com.tutorials.eu.favdish.databinding.FragmentDishDetailsBinding
 import java.io.IOException
@@ -42,12 +50,42 @@ class DishDetailsFragment : Fragment() {
         // TODO Step 10: Remove the log and populate the data to UI.
         // START
         args.let {
-
             try {
                 // Load the dish image in the ImageView.
                 Glide.with(requireActivity())
                     .load(it.dishDetails.image)
                     .centerCrop()
+                    .listener(object: RequestListener<Drawable>{
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            Log.e("TAG","ERROR Loading the image",e)
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            resource.let {
+                                Palette.from(resource!!.toBitmap()).generate(){
+                                        palette -> {
+                                            val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                                            mBinding!!.rlDishDetailMain.setBackgroundColor(intColor)
+                                        }
+                                }
+                            }
+                            return false
+
+                        }
+
+                    })
                     .into(mBinding!!.ivDishImage)
             } catch (e: IOException) {
                 e.printStackTrace()
